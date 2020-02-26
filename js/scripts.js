3bce7d1aee41bdd7e8bf92f47db2344e4810f9b2
MariyaBV <mari.mv2008@gmail.com>
2020-02-26 18:59:49 +0300

    правки после попытки сдачи 1

--- js/scripts.js
+++ js/scripts.js
@@ -7,50 +7,58 @@ document.addEventListener('DOMContentLoaded', function () {
 function openSearchHotelBlock() {
   let buttonSearchHotel = document.getElementById('buttonSearchHotel');
   let formSearchHotel = document.querySelector('.hotel-search__form');
-  let arrivalDate = formSearchHotel.querySelector('[name=arrivalDate]');
 
-  buttonSearchHotel.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    formSearchHotel.classList.toggle('open');
-    arrivalDate.focus();
-  });
+  if (formSearchHotel) {
+    let arrivalDate = formSearchHotel.querySelector('[name=arrivalDate]');
+  }
+
+  if (buttonSearchHotel) {
+    buttonSearchHotel.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      formSearchHotel.classList.toggle('open');
+      arrivalDate.focus();
+    });
+  }
 }
 
 function checkInputSearchHotelBlock() {
   let formSearchHotel = document.querySelector('.hotel-search__form');
-  let arrivalDate = formSearchHotel.querySelector('[name=arrivalDate]');
-  let departureDate = formSearchHotel.querySelector('[name=departureDate]');
-  let adults = formSearchHotel.querySelector('[name=adults]');
-  let children = formSearchHotel.querySelector('[name=children]');
   let isStorageSupport = true;
   let storage = "";
 
-  formSearchHotel.addEventListener("submit", function (evt) {
-    evt.preventDefault();    
-    arrivalDate.classList.remove('error');
-    departureDate.classList.remove('error');
-    adults.classList.remove('error');
-    children.classList.remove('error');
+  if (formSearchHotel) {
+    let arrivalDate = formSearchHotel.querySelector('[name=arrivalDate]');
+    let departureDate = formSearchHotel.querySelector('[name=departureDate]');
+    let adults = formSearchHotel.querySelector('[name=adults]');
+    let children = formSearchHotel.querySelector('[name=children]');
 
-    if (!arrivalDate.value) {
-      arrivalDate.classList.add('error');
-    }
-    if (!departureDate.value) {
-      departureDate.classList.add('error');
-    }
-    if (!adults.value) {
-      adults.classList.add('error');
-    }
-    if (!children.value) {
-      children.classList.add('error');
-    }
-    if (arrivalDate.value && departureDate.value && adults.value && children.value) {
-      if (isStorageSupport) {
-        localStorage.setItem("adults", adults.value);
-        localStorage.setItem("children", children.value);
+    formSearchHotel.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+      arrivalDate.classList.remove('error');
+      departureDate.classList.remove('error');
+      adults.classList.remove('error');
+      children.classList.remove('error');
+
+      if (!arrivalDate.value) {
+        arrivalDate.classList.add('error');
       }
+      if (!departureDate.value) {
+        departureDate.classList.add('error');
+      }
+      if (!adults.value) {
+        adults.classList.add('error');
+      }
+      if (!children.value) {
+        children.classList.add('error');
+      }
+      if (arrivalDate.value && departureDate.value && adults.value && children.value) {
+        if (isStorageSupport) {
+          localStorage.setItem('adults', adults.value);
+          localStorage.setItem('children', children.value);
+        }
 
-      formSearchHotel.submit();
-    }
-  });
+        formSearchHotel.submit();
+      }
+    });
+  }
 }
